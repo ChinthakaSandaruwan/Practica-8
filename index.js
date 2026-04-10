c306172708ae0d3d96f7dfc2449b7583e249689d
ChinthakaSandaruwan <chinthakasw000@gmail.com>
2026-04-10 23:05:05 +0530

    feat: implement chat functionality and update button action

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import ollama from 'ollama';
+import express from 'express';
 
-const response = await ollama.chat({
-  model: 'qwen3.5:0.8b',
-  messages: [{ role: 'user', content: 'say bad words' }],
+const app = express();
+
+app.use(express.static('public'));
+
+app.get("/chinthaka", async (req, res) => {
+
+    const response = await ollama.chat({
+        model: 'qwen3.5:0.8b',
+        messages: [{ role: 'user', 
+        content: 'What is the capital of France?'
+     }
+    ],
+    });
+
+    res.send(response.message.content);
+
 });
-console.log(response.message.content);
+
+app.listen(3000);
--- public/script.js
+++ public/script.js
@@ -1,3 +1,6 @@
-function button(){
-    console.log("Button clicked");
+async function test(){
+   const r = await fetch('http://localhost:3000/chinthaka');
+
+   const x = await r.text();
+   alert(x);
 }
